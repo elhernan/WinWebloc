a035f20a46de07be1e4bd3fd1728bd823c0548e8
elhernan <therealgillbates@gmail.com>
2014-09-14 20:47:32 -0500

    initial working version.

---
+++ WinWebloc/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("WinWebloc")> 
+<Assembly: AssemblyDescription("Opens OS X webloc files in default browser")> 
+<Assembly: AssemblyCompany("elhernan")> 
+<Assembly: AssemblyProduct("WinWebloc")> 
+<Assembly: AssemblyCopyright("Copyright © elhernan 2014")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("35351060-f878-438d-b03f-93f7b0cc374d")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
---
+++ WinWebloc/WinWebloc.vb
@@ -0,0 +1,55 @@
+﻿Imports System.IO
+Imports System.Text.RegularExpressions
+
+Module WinWebloc
+    Sub Main()
+        If My.Application.CommandLineArgs.Count = 0 Then
+            Console.WriteLine("No File dragged into the application's executable!")
+        Else
+            Dim filename = My.Application.CommandLineArgs.Item(0)
+            Dim s = GetFileContents(filename)
+            Dim url As String = ""
+            Try
+                If InStr(s, "<?xml") > 0 Then
+                    'new text style
+                    Dim xpos = InStr(InStr(s, "<dict>"), s, "http")
+                    If xpos > 0 Then
+                        s = Right(s, Len(s) - xpos + 1)
+                    End If
+                Else
+                    'old bin style
+                    Dim xpos = InStr(s, "http")
+                    If xpos > 0 Then
+                        s = Right(s, Len(s) - xpos + 1)
+                    End If
+                    s = Regex.Replace(s, "[\x01-\x1F]", "")
+                End If
+                For i = 1 To s.Length
+                    Dim c As Char = Mid(s, i, 1)
+                    If Convert.ToInt32(c) < 32 Or c = "<" Then
+                        Exit For
+                    End If
+                    url &= c
+                Next
+                If url.Length > 0 Then Process.Start(url)
+            Catch ex As Exception
+                Console.WriteLine(ex.ToString)
+            End Try
+        End If
+    End Sub
+
+
+    Private Function GetFileContents(ByVal filename As String) As String
+        Dim contents As String
+        Dim reader As StreamReader
+        Try
+            reader = New StreamReader(filename)
+            contents = reader.ReadToEnd
+            reader.Close()
+            Return contents
+        Catch ex As Exception
+            Return ""
+        End Try
+    End Function
+
+End Module
